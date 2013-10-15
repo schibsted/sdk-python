3246ebb9c635852da5637a9f317a4d4487528b8c
Frank Hammar <frank.hammar@blocket.se>
2013-10-15 09:40:21 +0200

    Initial setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+from spid import __version__
+
+long_description = ""
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    long_description = readme.read()
+
+setup(name="spid",
+    version=__version__,
+    description="SPiD library for Python",
+    long_description=long_description,
+    license="MIT",
+    author="SPiD",
+    author_email="support@schibstedpayment.no",
+    url="https://github.com/schibsted/sdk-python",
+    packages=find_packages(exclude=['tests']),
+    keywords="spid library",
+    zip_safe=True)
